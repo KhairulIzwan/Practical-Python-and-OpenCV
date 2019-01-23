40535edb76376b14b5f40e93689a2cf095c3ef41
Wansnap <wansnap@gmail.com>
2019-01-23 12:10:19 +0800

    commit new code on 23012019

--- drawing.py
+++ drawing.py
@@ -35,4 +35,14 @@ for r in range(0, 175, 25):
     cv2.circle(canvas, (centerX, centerY), r, white)
 
 cv2.imshow("Canvas", canvas)
-cv2.waitKey(0) 
+cv2.waitKey(0)
+
+for i in range(0, 25):
+    radius = np.random.randint(5, high=200)
+    color = np.random.randint(0, high=256, size=(3,)).tolist()
+    pt = np.random.randint(0, high=300, size=(2,))
+
+    cv2.circle(canvas, tuple(pt), radius, color, -1)
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
