f59899e6c3333bb01df977c87758581690c27701
Wansnap <wansnap@gmail.com>
2019-01-23 13:38:57 +0800

    Updated on 23012019 -- 13:38 pm

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # Practical Python and Opencv
-1.  load_display_save.py -- Understanding the concept how to load, display and save image.
-
-2.  getting and setting -- Understand how to manipulate an image.
-
-3.  drawing.py -- Understand how to draw(line, rectangle, circle).
-
-4.  translation.py -- Understand how to tranlate/move image to left right etc.
-
+1.  load_display_save.py
+2.  getting and setting
+3.  drawing.py
+4.  translation.py
 5.  rotate.py
---
+++ resize.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+cv2.waitKey(0)
+
+# resized with aspect ratio of width
+r = 150.0 / image.shape[1]
+dim = (150, int(image.shape[0] * r))
+
+resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+cv2.imshow("Resized (width)", resized)
+cv2.waitKey(0)
+
+# resized with aspect ratio of height
+r = 150.0 / image.shape[0]
+dim = (int(image.shape[1] * r), 150)
+
+resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+cv2.imshow("Resized (height)", resized)
+cv2.waitKey(0)
+
+resized = imutils.resize(image, width=150)
+cv2.imshow("Resized (width-imutils)", resized)
+cv2.waitKey(0)
+
+resized = imutils.resize(image, height=150)
+cv2.imshow("Resized (height-imutils)", resized)
+cv2.waitKey(0)
